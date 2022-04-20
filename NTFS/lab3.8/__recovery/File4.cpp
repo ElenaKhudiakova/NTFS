d59d10b0163ad1ac94d0d37cb17ce653aeca8c5d
Khudiakova Elena <87654857+ElenaKhudiakova@users.noreply.github.com>
2022-04-20 21:23:05 +0500

    Add files via upload

---
+++ NTFS/lab3.8/__recovery/File4.cpp
@@ -0,0 +1,109 @@
+#pragma hdrstop
+#pragma argsused
+
+//#include <fileapi.h>
+#include <windows.h>
+#include <iostream>
+#include <cstdlib>
+#include "NTFSDriverPCH1.h"
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale( LC_ALL,"Russian" );
+	BYTE databuffer[512];
+	NTFS_BootRecord *pNTFS_BootRecord = (NTFS_BootRecord*)databuffer;
+
+	HANDLE filehandle = CreateFileW(
+	L"\\\\.\\C:",
+	GENERIC_READ,
+	FILE_SHARE_READ | FILE_SHARE_WRITE,
+	NULL,
+	OPEN_EXISTING,
+	FILE_ATTRIBUTE_NORMAL,
+	NULL);
+
+	if (filehandle == INVALID_HANDLE_VALUE) {
+		std::cout << "Для корректного запуска файла требуются права администратора.\n";
+		system("pause");
+		exit(-1);
+	}
+
+	LARGE_INTEGER sectoroffset;
+	sectoroffset.QuadPart = 0;
+
+	unsigned long currentposition = SetFilePointer(
+	filehandle,
+	sectoroffset.LowPart,
+	&sectoroffset.HighPart,
+	FILE_BEGIN);
+
+	if (currentposition != sectoroffset.LowPart)
+	{
+		std::cout << "Offset error.\nTry again. :)\n";
+		system("pause");
+		exit(-1);
+	}
+
+	DWORD bytestoread = 512;
+	DWORD bytesread;
+
+	bool readresult = ReadFile(
+	filehandle,
+	databuffer,
+	bytestoread,
+	&bytesread,
+	NULL);
+
+	if(readresult == false || bytesread != bytestoread)
+	{
+		std::cout << "Error: The file cannot be read or there is a discrepancy in the number of bytes read.\nTry again. :)\n";
+		system("pause");
+		exit(-1);
+	}
+
+	UINT name_lo = pNTFS_BootRecord->name_lo;
+	UINT name_hi = pNTFS_BootRecord->name_hi;
+	USHORT sec_size = pNTFS_BootRecord->sec_size;
+	BYTE secs_cluster = pNTFS_BootRecord->secs_cluster;
+	ULONGLONG num_secs = pNTFS_BootRecord->num_secs;
+	ULONGLONG mft_clus = pNTFS_BootRecord->mft_clus;
+	ULONGLONG mft2_clus = pNTFS_BootRecord->mft2_clus;
+	UINT mft_rec_size = pNTFS_BootRecord->mft_rec_size;
+	UINT volume_id_lo = pNTFS_BootRecord->volume_id_lo;
+	UINT volume_id_hi = pNTFS_BootRecord->volume_id_hi;
+    USHORT sign = pNTFS_BootRecord->sign;
+
+	if (name_lo != 1397118030 && name_hi != 538976288) {
+		std::cout << "Error: The transition to the bootstrap program was not detected.\nTry again.\n";
+		system("pause");
+		exit(-1);
+	}
+	if (sign != 43605) {
+		std::cout << "Error: The signature not detected.\nTry again.\n";
+		system("pause");
+		exit(-1);
+	}
+
+	std::cout << "Number of bytes per sector: " << sec_size;
+	std::cout << "\nNumber of sectors per cluster: " << static_cast<int>(secs_cluster);
+	std::cout << "\nThe total number of sectors in the section: " << num_secs;
+	std::cout << "\nThe cluster number in which the MFT table begins: " << mft_clus;
+	std::cout << "\nThe cluster number in which the copy of the MFT table begins: " << mft2_clus;
+	std::cout << "\nMFT record size in clusters: " << mft_rec_size;
+	std::cout << "\nThe lowest word of the disk serial number: " << volume_id_lo;
+	std::cout << "\nThe highest word of the disk serial number: " << volume_id_hi;
+
+	std::cout << "\n";
+	system("pause");
+	CloseHandle(filehandle);
+	return 0;
+}
